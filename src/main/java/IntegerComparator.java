91a5d0b814cccf851b1e1426e9f7d8325634f169
tusharmelmalgi <tushar.melmalgi@capillarytech.com>
2022-06-28 12:47:44 +0530

    Modified IntegerComparator to pass testcase 3

--- src/main/java/IntegerComparator.java
+++ src/main/java/IntegerComparator.java
@@ -2,6 +2,8 @@ public class IntegerComparator {
     public int checkNum(int number1,int number2){
         if(number1==number2)
             return 0;
+        if(number1>number2)
+            return 1;
         return -1;
     }
 }
