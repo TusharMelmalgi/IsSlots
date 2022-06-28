a346dfeba5d35caaf63b64ddfa20b54ddc9f9ad5
tusharmelmalgi <tushar.melmalgi@capillarytech.com>
2022-06-28 12:13:51 +0530

    Creating Integer Comparison using TTD case1

---
+++ src/main/java/IntegerComparator.java
@@ -0,0 +1,5 @@
+public class IntegerComparator {
+    public int checkNum(int number1,int number2){
+        return -1;
+    }
+}
---
+++ src/test/java/IntegerComparatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IntegerComparatorTest {
+    /* when number1 less than number2 return -1
+    when number1 equals number2 return 0
+    when number1 greater than number2 return 1
+     */
+    @Test
+    public void compareTest_number1LessThanNumber2(){
+        int number1 = 5;
+        int number2 = 10;
+        IntegerComparator compares = new IntegerComparator();
+        int resultOfCompare = compares.checkNum(number1,number2);
+        Assertions.assertEquals(-1,resultOfCompare);
+    }
+
+}
